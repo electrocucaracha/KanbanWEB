c956df0c9446ac610e9410eb6c075c29ad9c1d94
Victor Morales <victor.morales@intel.com>
2014-06-10 21:25:05 -0500

    Usage of JavaServer Pages
    Instead of using servlet to catch get http methods, now it was changed
    this code to use jsp files and compare their advantages.

---
+++ WebContent/AddTaskForm.jsp
@@ -0,0 +1,55 @@
+<%@ page language="java" contentType="text/html"%>
+<%@ page import="java.util.Date"%>
+<%@ page import="mx.edu.umg.personalkaban.model.State"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Add task</title>
+<%@ include file="Copyright.jspf"%>
+</head>
+<body>
+	<form method="POST" action="AddTaskServlet">
+		<table>
+			<tr>
+				<td>Title:</td>
+				<td><input type="text" name="title" /></td>
+			</tr>
+			<tr>
+				<td>Description:</td>
+				<td><input type="text" name="description" /></td>
+			</tr>
+			<tr>
+				<td>State:</td>
+				<td><select name="state">
+						<%-- State values added dynamically --%>
+						<%		for (State state : State.values()) { %>
+						<option value="<%= state.name() %>">
+							<%= state %></option>
+						<% } %>
+				</select></td>
+			</tr>
+			<tr>
+				<td>Category:</td>
+				<td><input type="text" name="category" /></td>
+			</tr>
+			<tr>
+				<td>Priority:</td>
+				<td><input type="text" name="priority" /></td>
+			</tr>
+			<tr>
+				<td>Owner:</td>
+				<td><input type="text" name="owner" /></td>
+			</tr>
+			<tr>
+				<td>Due date:</td>
+				<td><input type="text" name="due_date"
+					value="<%= new Date().toString()%>" /></td>
+			</tr>
+			<tr>
+				<td colspan="2"><input type="submit" value="Add" /></td>
+			</tr>
+		</table>
+	</form>
+</body>
+</html>
--- src/mx/edu/umg/personalkanban/view/AddTaskServlet.java
+++ src/mx/edu/umg/personalkanban/view/AddTaskServlet.java
@@ -19,62 +19,6 @@ import mx.edu.umg.personalkaban.model.Task;
 public class AddTaskServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	protected void doGet(HttpServletRequest request,
-			HttpServletResponse response) throws ServletException, IOException {
-		response.setContentType("text/html");
-		PrintWriter writer = response.getWriter();
-		writer.println("<html>");
-		writer.println("	<head>");
-		writer.println("		<title>Add task</title>");
-		writer.println("	</head>");
-		writer.println("	<body>");
-		writer.println("		<form method=\"POST\">");
-		writer.println("			<table>");
-		writer.println("				<tr>");
-		writer.println("					<td>Title:</td>");
-		writer.println("					<td><input type=\"text\" name=\"title\" /></td>");
-		writer.println("				</tr>");
-		writer.println("				<tr>");
-		writer.println("					<td>Description:</td>");
-		writer.println("					<td><input type=\"text\" name=\"description\" /></td>");
-		writer.println("				</tr>");
-		writer.println("				<tr>");
-		writer.println("					<td>State:</td>");
-		writer.println("					<td>");
-		writer.println("						<select name=\"state\">");
-		for (State state : State.values()) {
-			writer.println("							<option value=\"" + state.name()
-					+ "\">" + state.toString() + "</option>");
-		}
-		writer.println("						</select>");
-		writer.println("					</td>");
-		writer.println("				</tr>");
-		writer.println("				<tr>");
-		writer.println("					<td>Category:</td>");
-		writer.println("					<td><input type=\"text\" name=\"category\" /></td>");
-		writer.println("				</tr>");
-		writer.println("				<tr>");
-		writer.println("					<td>Priority:</td>");
-		writer.println("					<td><input type=\"text\" name=\"priority\" /></td>");
-		writer.println("				</tr>");
-		writer.println("				<tr>");
-		writer.println("					<td>Owner:</td>");
-		writer.println("					<td><input type=\"text\" name=\"owner\" /></td>");
-		writer.println("				</tr>");
-		writer.println("				<tr>");
-		writer.println("					<td>Due date:</td>");
-		writer.println("					<td><input type=\"text\" name=\"due_date\" value=\""
-				+ new Date() + "\"/></td>");
-		writer.println("				</tr>");
-		writer.println("				<tr>");
-		writer.println("					<td colspan=\"2\"><input type=\"submit\" value=\"Add\" /></td>");
-		writer.println("				</tr>");
-		writer.println("			</table>");
-		writer.println("		</form>");
-		writer.println("	</body>");
-		writer.println("</html>");
-	}
-
 	protected void doPost(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
 
